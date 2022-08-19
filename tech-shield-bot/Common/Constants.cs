a0af621d2b771d53d6ad38ceaa30b45c8066fbb9
harunugurlu <harunugurlu1@gmail.com>
2022-08-19 22:18:27 +0300

    add tech-shield-bot

---
+++ tech-shield-bot/Common/Constants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CallingBotSample.Common
+{
+    public class Constants
+    {
+
+        public const string ClientIdConfigurationSettingsKey = "AzureAd:ClientId";
+
+        public const string TenantIdConfigurationSettingsKey = "AzureAd:TenantId";
+
+        public const string ClientSecretConfigurationSettingsKey = "AzureAd:ClientSecret";
+
+        public const string UserIdConfigurationSettingsKey = "UserId";
+
+        public const string MicrosoftAppPasswordConfigurationSettingsKey = "MicrosoftAppPassword";
+
+        public const string MicrosoftAppIdConfigurationSettingsKey = "MicrosoftAppId";
+
+        public const string BotBaseUrlConfigurationSettingsKey = "BotBaseUrl";
+    }
+}
